c1769067061170e59265a924d79c177e982c1fb6
Vedanshee Trivedi <119843736+Vedanshee03@users.noreply.github.com>
2024-07-12 10:13:45 -0700

    Update obstacle_detection_node.py

--- src/sweeper_bot/scripts/obstacle_detection_node.py
+++ src/sweeper_bot/scripts/obstacle_detection_node.py
@@ -11,10 +11,11 @@ from cv_bridge import CvBridge, CvBridgeError
 import threading
 
 class ObstacleDetectionNode(Node):
-    def __init__(self):
-        super().__init__('obstacle_detection_node')
+    def _init_(self):
+        super()._init_('obstacle_detection_node')
         self.bridge = CvBridge()
         self.create_subscription(Image, '/camera/realsense2_camera/depth/image_rect_raw', self.depth_callback, 10)
+        #self.create_subscription(Image, '/camera/camera/depth/image_rect_raw', self.depth_callback, 10)
         self.pub_cmd_vel = self.create_publisher(Twist, 'cmd_vel', 10)
 
         # Set up logging
@@ -35,11 +36,6 @@ class ObstacleDetectionNode(Node):
         # Placeholder for the previous command 
         self.prev_cmd = Twist()
 
-        # Flags to indicate obstacle detection
-        self.obstacle_left = False
-        self.obstacle_right = False
-        self.turning = False
-
     def depth_callback(self, msg):
         try:
             # Convert ROS Image message to OpenCV image
@@ -56,9 +52,6 @@ class ObstacleDetectionNode(Node):
         left_image = self.latest_depth_image[:, :width//2]
         right_image = self.latest_depth_image[:, width//2:]
 
-        self.obstacle_left = False
-        self.obstacle_right = False
-
         obstacles = {'left': False, 'right': False}
 
         for region_name, region in {'left': left_image, 'right': right_image}.items():
@@ -71,19 +64,23 @@ class ObstacleDetectionNode(Node):
         
         if obstacles['left'] and not obstacles['right']:
             self.get_logger().info('Obstacle detected on the left, turning right.')
-            self.obstacle_left = True
-            self.start_turning('right')
+            #self.logger.info('Obstacle detected on the left, turning right.')
+            self.obstacle_left = True 
+            self.full_turn_right()
         elif obstacles['right'] and not obstacles['left']:
             self.get_logger().info('Obstacle detected on the right, turning left.')
+            #self.logger.info('Obstacle detected on the right, turning left.')
             self.obstacle_right = True
-            self.start_turning('left')
+            self.full_turn_left()
         elif obstacles['left'] and obstacles['right']:
             self.get_logger().info('Obstacles detected on both sides. Stop')
+            #self.logger.info('Obstacles detected on both sides. Stop')
             self.obstacle_left = True
             self.obstacle_right = True
-            self.stop()
+            self.move_backward_and_turn_right()
         else:
-            self.get_logger().info('No obstacles detected, moving forward.')
+            self.get_logger().info('No obstacles detected, performing random movement.')
+            #self.logger.info('No obstacles detected, performing random movement.')
             self.move_forward()
 
     def move_forward(self):
@@ -92,70 +89,75 @@ class ObstacleDetectionNode(Node):
         cmd.angular.z = 0.0
         self.pub_cmd_vel.publish(cmd)
         self.get_logger().info('Command: Move Forward')
-        self.turning = False
+        #self.logger.info('Command: Move forward')
 
     def turn_left(self):
         cmd = Twist()
         cmd.linear.x = 0.0
-        cmd.angular.z = 0.2  # Slow turn
+        cmd.angular.z = 1.0
         self.pub_cmd_vel.publish(cmd)
         self.get_logger().info('Command: Turn Left')
-
+        #self.logger.info('Command: Turn left')
+       
     def turn_right(self):
         cmd = Twist()
         cmd.linear.x = 0.0
-        cmd.angular.z = -0.2  # Slow turn
+        cmd.angular.z = -1.0
         self.pub_cmd_vel.publish(cmd)
         self.get_logger().info('Command: Turn Right')
-
+        #self.logger.info('Command: Turn right')
+       
     def stop(self):
         cmd = Twist()
         cmd.linear.x = 0.0
         cmd.angular.z = 0.0
         self.pub_cmd_vel.publish(cmd)
         self.get_logger().info('Command: Stop')
-        self.turning = False
+        #self.logger.info('Command: Stop')
 
-    def start_turning(self, direction):
-        self.turning = True
-        if direction == 'left':
-            self.turn_left()
-        elif direction == 'right':
-            self.turn_right()
+    def move_backward(self):
+        cmd = Twist()
+        cmd.linear.x = -0.7 
+        cmd.angular.z = 0.0
+        self.pub_cmd_vel.publish(cmd)
+        self.get_logger().info('Command: Move Backward')
+        #self.logger.info('Command: Move Backward')
+    def spot_turn_right(self):
+        cmd = Twist()
+        cmd.linear.x = 0.0
+        cmd.angular.z = -2.0
+        self.pub_cmd_vel.publish(cmd)
+        self.get_logger().info('Command: Spot turning to right')
 
-        threading.Timer(0.5, self.check_turning_obstacle).start()
+    def spot_turn_left(self):
+        cmd = Twist()
+        cmd.linear.x = 0.0
+        cmd.angular.z = 2.0
+        self.pub_cmd_vel.publish(cmd)
+        self.get_logger().info('Command: Spot turning to left')
+        
+    def full_turn_right(self):
+        self.stop()
+        threading.Timer(0.5, self.spot_turn_right).start()
+        threading.Timer(3.0, self.move_forward).start()
+        
+    def full_turn_left(self):
+        self.stop()
+        threading.Timer(0.5, self.spot_turn_left).start()
+        threading.Timer(3.0, self.move_forward).start()
 
-    def check_turning_obstacle(self):
-        if self.latest_depth_image is None:
-            return
-
-        # Split the depth image into left and right parts
-        height, width = self.latest_depth_image.shape
-        left_image = self.latest_depth_image[:, :width//2]
-        right_image = self.latest_depth_image[:, width//2:]
-
-        obstacles = {'left': False, 'right': False}
-
-        for region_name, region in {'left': left_image, 'right': right_image}.items():
-            valid_region_depths = region[region > 0]
-            if valid_region_depths.size > 0:
-                min_region_depth = np.min(valid_region_depths)
-                if min_region_depth < 500:
-                    obstacles[region_name] = True
-
-        if not obstacles['left'] and not obstacles['right']:
-            self.move_forward()
-        else:
-            if self.obstacle_left:
-                self.start_turning('right')
-            elif self.obstacle_right:
-                self.start_turning('left')
+    def move_backward_and_turn_right(self):
+        self.stop()
+        threading.Timer(0.5, self.move_backward).start()
+        threading.Timer(1.0, self.spot_turn_right).start()
+        threading.Timer(3.5, self.move_forward).start()
 
     def publish_cmd(self, cmd):
         if cmd.linear.x != self.prev_cmd.linear.x or cmd.angular.z != self.prev_cmd.angular.z:
             self.prev_cmd = cmd 
             self.pub_cmd_vel.publish(cmd)
 
+    
     def display_images(self):
         while True:
             if self.latest_depth_image is None:
@@ -189,5 +191,5 @@ def main(args=None):
     rclpy.shutdown()
     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     main()
