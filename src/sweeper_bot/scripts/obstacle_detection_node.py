a5a76c30ed79ef9223ee290f0e54624e1a4c3fd6
Vedanshee Trivedi <119843736+Vedanshee03@users.noreply.github.com>
2024-07-12 09:14:28 -0700

    Updated spot turn left and right

--- src/sweeper_bot/scripts/obstacle_detection_node.py
+++ src/sweeper_bot/scripts/obstacle_detection_node.py
@@ -65,15 +65,15 @@ class ObstacleDetectionNode(Node):
         if obstacles['left'] and not obstacles['right']:
             self.get_logger().info('Obstacle detected on the left, turning right.')
             #self.logger.info('Obstacle detected on the left, turning right.')
-            self.turn_right()
+            self.full_turn_right()
         elif obstacles['right'] and not obstacles['left']:
             self.get_logger().info('Obstacle detected on the right, turning left.')
             #self.logger.info('Obstacle detected on the right, turning left.')
-            self.turn_left()
+            self.full_turn_left()
         elif obstacles['left'] and obstacles['right']:
             self.get_logger().info('Obstacles detected on both sides. Stop')
             #self.logger.info('Obstacles detected on both sides. Stop')
-            self.stop_and_turn_back()
+            self.move_backward_and_turn_right()
         else:
             self.get_logger().info('No obstacles detected, performing random movement.')
             #self.logger.info('No obstacles detected, performing random movement.')
@@ -124,19 +124,36 @@ class ObstacleDetectionNode(Node):
         cmd.angular.z = -2.0
         self.pub_cmd_vel.publish(cmd)
         self.get_logger().info('Command: Spot turning to right')
+
+    def spot_turn_left(self):
+        cmd = Twist()
+        cmd.linear.x = 0.0
+        cmd.angular.z = 2.0
+        self.pub_cmd_vel.publish(cmd)
+        self.get_logger().info('Command: Spot turning to left')
         
+    def full_turn_right(self):
+        self.stop()
+        threading.Timer(0.5, self.spot_turn_right).start()
+        threading.Timer(3.0, self.move_forward).start()
+        
+    def full_turn_left(self):
+        self.stop()
+        threading.Timer(0.5, self.spot_turn_left).start()
+        threading.Timer(3.0, self.move_forward).start()
+
+    def move_backward_and_turn_right(self):
+        self.stop()
+        threading.Timer(0.5, self.move_backward).start()
+        threading.Timer(1.0, self.spot_turn_right).start()
+        threading.Timer(3.5, self.move_forward).start()
 
     def publish_cmd(self, cmd):
         if cmd.linear.x != self.prev_cmd.linear.x or cmd.angular.z != self.prev_cmd.angular.z:
             self.prev_cmd = cmd 
             self.pub_cmd_vel.publish(cmd)
 
-    def stop_and_turn_back(self):
-        self.stop()
-        threading.Timer(1.5, self.move_backward).start()
-        threading.Timer(3.0, self.spot_turn_right).start()
-        threading.Timer(4.5, self.move_forward).start()
-
+    
     def display_images(self):
         while True:
             if self.latest_depth_image is None:
