0b7702ed1a7ab02ea878017d279f211057c8f792
AlexanderRoller <alexander.roller12@gmail.com>
2024-07-23 10:50:37 -0700

    Remove Realsense

--- src/sweeper_bot/launch/sweepy_launch.py
+++ src/sweeper_bot/launch/sweepy_launch.py
@@ -11,7 +11,6 @@ def generate_launch_description():
     pkg_share = FindPackageShare(package='sweeper_bot').find('sweeper_bot')
     default_model_path = os.path.join(pkg_share, 'src', 'description', 'robot.urdf.xacro')
     default_rviz_config_path = os.path.join(pkg_share, 'config', 'urdf_config.rviz')
-    realsense_params_file = os.path.join(get_package_share_directory('sweeper_bot'), 'config', 'realsense_params.yaml')
     ekf_params_file = os.path.join(get_package_share_directory('sweeper_bot'), 'config', 'ekf_params.yaml')
 
     robot_description_content = Command(['xacro ', LaunchConfiguration('model')])
@@ -38,25 +37,6 @@ def generate_launch_description():
         name='rviz2',
         output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
-    )
-
-    realsense_node = Node(
-        package='realsense2_camera',
-        executable='realsense2_camera_node',
-        name='realsense2_camera',
-        output='screen',
-        parameters=[realsense_params_file, {'pointcloud.enable': True}, {'depth_module.global_time_enabled': True}],
-        remappings=[
-            ('/camera/realsense2_camera/gyro/sample', '/camera/realsense2_camera/accel/sample'),
-            ('/camera/realsense2_camera/accel/sample', '/camera/realsense2_camera/gyro/sample')
-        ]
-    )
-
-    imu_corrector_node = Node(
-        package='sweeper_bot',
-        executable='imu_data_corrector.py',
-        name='imu_data_corrector',
-        output='screen'
     )
 
     ekf_node = Node(
@@ -113,16 +93,6 @@ def generate_launch_description():
             remappings=[('/cmd_vel_out','/diffbot_base_controller/cmd_vel_unstamped')]
         )
     
-    laserscan_params = os.path.join(get_package_share_directory('sweeper_bot'), 'config', 'laserscan_params.yaml')
-    laser_scan =Node(
-            package='depthimage_to_laserscan',
-            executable='depthimage_to_laserscan_node',
-            name='depthimage_to_laserscan',
-            remappings=[('depth', '/camera/realsense2_camera/depth/image_rect_raw'),
-                        ('depth_camera_info', '/camera/realsense2_camera/depth/camera_info')],
-            parameters=[laserscan_params]
-        ) 
-    
     sick_scan_pkg_prefix = get_package_share_directory('sick_scan_xd')
     tim_launch_file_path = os.path.join(sick_scan_pkg_prefix, 'launch/sick_tim_7xx.launch')
     
@@ -143,13 +113,10 @@ def generate_launch_description():
         #joint_state_publisher_node,
         rsp,
         rviz_node,
-        #realsense_node,
         ekf_node,
-        #imu_corrector_node,
         ros2_control_node,
         joint_state_broadcaster_spawner,
         robot_controller_spawner,
         twist_mux,
-        #laser_scan,
         sick_node,
     ])
